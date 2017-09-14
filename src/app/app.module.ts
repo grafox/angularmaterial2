cfca9ef91d249ba20a7b3263e9dad4eca412e675
grafox <g_fox_fts@yahoo.com>
2017-09-14 15:31:35 +0200

    step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MdToolbarModule,MdMenuModule,MdButtonModule,MdIconModule,MdInputModule,MdCardModule} from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MdToolbarModule,MdMenuModule,MdButtonModule,MdIconModule,MdInputModule,MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
